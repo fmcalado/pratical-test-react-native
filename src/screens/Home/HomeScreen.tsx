fa524a03ad30358aec7d9078051e556b778ea57e
Francisco Calado <wishsolidor@gmail.com>
2022-08-26 22:10:44 -0300

    style homescreen

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,10 +1,105 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useQuery } from "@tanstack/react-query";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView } from "react-native";
 
-import { Container } from './styles';
+import {
+  Body,
+  Container,
+  Header,
+  HeaderAvatar,
+  HeaderTitle,
+  SearchField,
+  SearchFieldPlaceholder,
+  SearchFieldIcon,
+  HeadingTitle,
+  BookList,
+  CategoryList,
+} from "./styles";
+import { api, BookListData, CategoryItem } from "../../service/api";
+import CategoryCard from "../../components/CategoryCard";
+import BookCard from "../../components/BookCard";
+import { mockBooks } from "../../Mock/books";
 
 const HomeScreen: React.FC = () => {
-  return <View />;
-}
+  const { data: bookLists } = useQuery(
+    ["books-overview-list"],
+    api.getBookLists,
+    { initialData: mockBooks }
+  );
+  const { data: categories } = useQuery(
+    ["categories-list"],
+    api.getCategories,
+    { initialData: [] }
+  );
+
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Container>
+        <ScrollView>
+          <Header>
+            <HeaderTitle>BookShelf</HeaderTitle>
+            <HeaderAvatar source={{ uri: "https://github.com/fmcalado.png" }} />
+          </Header>
+
+          <Body>
+            <SearchField>
+              <SearchFieldPlaceholder>
+                Qual livro você gostaria de ler hoje?
+              </SearchFieldPlaceholder>
+              <SearchFieldIcon
+                source={require("../../assets/icons/search.png")}
+              />
+            </SearchField>
+
+            <HeadingTitle>Pra você</HeadingTitle>
+            <BookList
+              horizontal
+              data={bookLists}
+              renderItem={({ item: bookList }: { item: BookListData }) => (
+                <BookCard
+                  author={bookList.books[0].author}
+                  category={bookList.display_name}
+                  categoryEncoded={bookList.list_name_encoded}
+                  description={bookList.books[0].description}
+                  imageUrl={bookList.books[0].book_image}
+                  title={bookList.books[0].title}
+                />
+              )}
+            />
+
+            <HeadingTitle>Categorias</HeadingTitle>
+            <CategoryList
+              horizontal
+              data={categories}
+              renderItem={({ item }: { item: CategoryItem }) => (
+                <CategoryCard
+                  name={item.display_name}
+                  encodedName={item.list_name_encoded}
+                />
+              )}
+            />
+
+            <HeadingTitle>Os mais lidos da semana</HeadingTitle>
+            <BookList
+              horizontal
+              data={bookLists}
+              renderItem={({ item: bookList }: { item: BookListData }) => (
+                <BookCard
+                  author={bookList.books[0].author}
+                  category={bookList.display_name}
+                  categoryEncoded={bookList.list_name_encoded}
+                  description={bookList.books[0].description}
+                  imageUrl={bookList.books[0].book_image}
+                  title={bookList.books[0].title}
+                />
+              )}
+            />
+          </Body>
+        </ScrollView>
+      </Container>
+    </>
+  );
+};
 
 export default HomeScreen;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -5,3 +5,77 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.white};
 `;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(56)}px;
+  background-color: ${({ theme }) => theme.colors.white};
+
+  padding: 12px 24px 12px 24px; /* top right bottom left */
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.black};
+  font-size: ${RFValue(24)}px;
+  line-height: ${RFValue(32)}px;
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+export const HeaderAvatar = styled.Image`
+  width: ${RFValue(32)}px;
+  height: ${RFValue(32)}px;
+  border-radius: 100%;
+  background-color: ${({ theme }) => theme.colors.gray3};
+`;
+
+export const Body = styled.View`
+  height: 100%;
+  width: 100%;
+  padding: ${RFValue(16)}px;
+`;
+
+export const SearchField = styled.View`
+  width: 100%;
+  height: ${RFValue(56)}px;
+  padding: 20px;
+
+  border: 1px solid;
+  border-color: ${({ theme }) => theme.colors.gray6};
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: ${RFValue(24)}px; ;
+`;
+
+export const SearchFieldPlaceholder = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  line-height: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.gray3};
+`;
+
+export const SearchFieldIcon = styled.Image`
+  width: ${RFValue(24)}px;
+  height: ${RFValue(24)}px;
+`;
+
+export const HeadingTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.black};
+  font-size: ${RFValue(18)}px;
+  line-height: ${RFValue(21.6)}px;
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+export const BookList = styled.FlatList`
+  margin-top: ${RFValue(16)}px;
+  margin-bottom: ${RFValue(32)}px;
+`;
+
+export const CategoryList = styled.FlatList`
+  margin-top: ${RFValue(16)}px;
+`;
