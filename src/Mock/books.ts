79d60d0bcc0b01a82dc12a5909b0a6dc117deee7
Francisco Calado <wishsolidor@gmail.com>
2022-08-26 22:04:39 -0300

    create fake data to show loading state

---
+++ src/Mock/books.ts
@@ -0,0 +1,28 @@
+export const mockBooks = [{
+  display_name: 'Carregando',
+  list_name_encoded: 'Carregando',
+  books: [{
+    book_image: 'https://www.ufpb.br/ppgq/contents/imagens/loading_balao.jpg/@@images/image.gif',
+    title: 'CARREGANDO',
+    author: 'CARREGANDO',
+    description: 'CARREGANDO',
+  }],
+},{
+  display_name: 'Carregando',
+  list_name_encoded: 'Carregando',
+  books: [{
+    book_image: 'https://www.ufpb.br/ppgq/contents/imagens/loading_balao.jpg/@@images/image.gif',
+    title: 'CARREGANDO',
+    author: 'CARREGANDO',
+    description: 'CARREGANDO',
+  }],
+},{
+  display_name: 'Carregando',
+  list_name_encoded: 'Carregando',
+  books: [{
+    book_image: 'https://www.ufpb.br/ppgq/contents/imagens/loading_balao.jpg/@@images/image.gif',
+    title: 'CARREGANDO',
+    author: 'CARREGANDO',
+    description: 'CARREGANDO',
+  }],
+}]
